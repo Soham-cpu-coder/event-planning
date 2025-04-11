d2fa41e152ed90c02df92d72183b022e2239ac3f
Soham-cpu-coder <sohammali715@gmail.com>
2025-04-11 12:08:12 +0530

    Add files via upload

---
+++ events2.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Bootstrap Web Design</title>
+        <?php require 'utils/styles.php'; ?><!--css links. file found in utils folder-->
+        <?php require 'utils/scripts.php'; ?><!--js links. file found in utils folder-->
+    </head>
+    <body>
+        <?php require 'utils/header.php'; ?><!--header content. file found in utils folder-->
+        <div class="content"><!--body content holder-->
+            <div class="container">
+                <div class="col-md-12"><!--body content title holder with 12 grid columns-->
+                    <h1>What's On</h1><!--body content title-->
+                </div>
+            </div>
+			
+            <div class="container">
+            <div class="col-md-12">
+            <hr>
+            </div>
+            </div>
+			
+            <div class="row"><!--event content-->
+                <section>
+                    <div class="container">
+                        <div class="date col-md-1"><!--date holder with 1 grid column-->
+                            <span class="month">JAN</span><br><!--month-->
+                            <hr class="line"><!--css modified horizontal line-->
+                            <span class="day">20</span><!--day-->
+                        </div>
+                        <div class="col-md-5"><!--image holder with 5 grid column-->
+                            <img src="images/bdayevent.jpg" class="img-responsive">
+                        </div>
+                        <div class="subcontent col-md-6"><!--event content holder with 6 grid column-->
+                            <h1 class="title">Joe's 21st</h1><!--event content title-->
+                            <p class="location"><!--event content location-->
+                            Bastian, Bandra
+                            </p>
+                            <p class="definition"><!--event content definition-->
+                             Welcome to the world of official adulthood—where the drinks are legal,
+                              but the responsibilities get real! Wishing you an unforgettable night filled with laughter, good vibes, and maybe just a little bit of chaos.
+                              Enjoy every moment—tonight is all about YOU! Cheers to 21!
+                            </p>
+                            <hr class="customline2"><!--css modified horizontal line-->
+                            <button type="button" class="btn btn-default btn-lg"><!--view details button (no function implemented)-->
+                            View Details  <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span><!--arrow right glyphicon-->
+                            </button>
+                        </div><!--subcontent div-->
+                    </div><!--container div-->
+                </section>
+            </div><!--row div-->
+			
+            <div class="container">
+            <div class="col-md-12">
+            <hr>
+            </div>
+            </div>
+
+            <div class="row">
+                <section>
+                    <div class="container">
+                        <div class="date col-md-1"><!--date holder with 1 grid column-->
+                            <span class="month">APR</span><br><!--month-->
+                            <hr class="line"><!--css modified horizontal line-->
+                            <span class="day">20</span><!--day-->
+                        </div>
+                        <div class="col-md-5"><!--image holder with 5 grid column-->
+                            <img src="images/fashevent.jpg" class="img-responsive">
+                        </div>
+                        <div class="subcontent col-md-6"><!--event content holder with 6 grid column-->
+                            <h1 class="title">Dress to Impress</h1><!--event content title-->
+                            <p class="location"><!--event content location-->
+                            Mystery Bar, Kalyan
+                            </p>
+                            <p class="definition"><!--event content definition-->
+                            "Get ready to turn heads! 🔥✨ This is your chance to bring out your best look—because at Joe’s 21st, 
+                            the dress code is DRESS TO IMPRESS! Suit up, glam up, and let’s celebrate in style. See you there! 🍾🥂"
+                            </p>
+                            <hr class="customline2"><!--css modified horizontal line-->
+                            <button type="button" class="btn btn-default btn-lg"><!--view details button (no function implemented)-->
+                            View Details  <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span><!--arrow right glyphicon-->
+                            </button>
+                        </div><!--subcontent div-->
+                    </div><!--container div-->
+                </section>
+            </div><!--row div-->
+			
+            <div class="container">
+            <div class="col-md-12">
+                <hr>
+            </div>
+            </div>
+
+            <div class="row">
+                <section>
+                    <div class="container">
+                        <div class="date col-md-1"><!--date holder with 1 grid column-->
+                            <span class="month">JUN</span><br><!--month-->
+                            <hr class="line"><!--css modified horizontal line-->
+                            <span class="day">20</span><!--day-->
+                        </div>
+                        <div class="col-md-5"><!--image holder with 5 grid column-->
+                            <img src="images/wedevent.jpg" class="img-responsive">
+                        </div>
+                        <div class="subcontent col-md-6"><!--event content holder with 6 grid column-->
+                            <h1 class="title">Our 2nd Anniversary</h1><!--event content title-->
+                            <p class="location"><!--event content location-->
+                            Galaxy Banquet Hall, Belapur
+                            </p>
+                            <p class="definition"><!--event content definition-->
+                             Two years down, a lifetime to go! Whether it’s been laughter, challenges, or unforgettable moments, every second has been worth it. 
+                            Here’s to growing stronger, loving deeper, and creating even more beautiful memories together. Cheers to us and to many more anniversaries ahead! 
+                            </p>
+                            <hr class="customline2"><!--css modified horizontal line-->
+                            <button type="button" class="btn btn-default btn-lg"><!--view details button (no function implemented)-->
+                            View Details  <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span><!--arrow right glyphicon-->
+                            </button>
+                        </div><!--subcontent div-->
+                    </div><!--container div-->
+                </section>
+            </div><!--row div-->
+			
+            <div class="container">
+            <div class="col-md-12">
+                <hr>
+            </div>
+            </div>
+
+            <div class="row">
+                <section>
+                    <div class="container">
+                        <div class="date col-md-1"><!--date holder with 1 grid column-->
+                            <span class="month">AUG</span><br><!--month-->
+                            <hr class="line"><!--css modified horizontal line-->
+                            <span class="day">20</span><!--day-->
+                        </div>
+                        <div class="col-md-5"><!--image holder with 5 grid column-->
+                            <img src="images/meetevent.jpg" class = "img-responsive">
+                        </div>
+                        <div class="subcontent col-md-6"><!--event content holder with 6 grid column-->
+                            <h1 class="title">Career Talk</h1><!--event content title-->
+                            <p class="location"><!--event content location-->
+                            The Vista Room,Kharghar
+                            </p>
+                            <p class="definition"><!--event content definition-->
+                             Discover insights, strategies, and real-world advice from industry experts who have been where you are and know what it takes to succeed.
+                             Whether you're exploring career paths, seeking motivation, or looking to level up your professional game, this is an opportunity you don’t want to miss
+                            </p>
+                            <hr class="customline2"><!--css modified horizontal line-->
+                            <button type="button" class="btn btn-default btn-lg"><!--view details button (no function implemented)-->
+                                View Details  <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span><!--arrow right glyphicon-->
+                            </button>
+                       </div><!--subcontent div-->
+                    </div><!--container div-->
+                </section>
+            </div><!--row div-->
+        </div><!--body content div-->
+        <?php require 'utils/footer.php'; ?><!--footer content. file found in utils folder-->
+    </body>
+</html>
